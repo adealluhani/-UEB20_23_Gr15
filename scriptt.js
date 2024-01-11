af4672f86ecdeb758ddf71c1bb878b5e9778500d
RozetaMeshi <122020803+RozetaMeshi@users.noreply.github.com>
2024-01-11 21:27:54 +0100

    Update scriptt.js

--- scriptt.js
+++ scriptt.js
@@ -1,18 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Cakto një kohë fillestare
-    const startTime = new Date().getTime();
-  
-    // Përdor setTimeout për të azhurnuar kohën çdo sekondë
-    setTimeout(function updateDuration() {
-      const currentTime = new Date().getTime();
-      const elapsedSeconds = Math.floor((currentTime - startTime) / 1000);
-  
-      // Shfaq kohën e qëndrimit
-      const timeElapsedElement = document.getElementById("timeElapsed");
-      timeElapsedElement.textContent = `${elapsedSeconds} seconds`;
-  
-      // Rifresko kohën çdo sekondë
-      setTimeout(updateDuration, 1000);
-    }, 1000);
-  });
-  
+
